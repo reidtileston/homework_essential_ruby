b42edfd87300dc9e953261eeb95f8904644148a8
reidtileston <rtilesto@chicagobooth.edu>
2014-10-27 16:46:42 -0500

    Moving Forward

--- calc.rb
+++ calc.rb
@@ -1,2 +1,6 @@
+puts 'hello there, what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name'
+puts 'Pleased to meet you, ' + name + ':)'
 
-puts 'you\'re the best'
+
